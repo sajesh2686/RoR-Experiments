6f1171b4d790062dafc007a541b54d91aaac692d
Sajesh <darkknight@Sajesh.(none)>
2014-03-04 19:47:18 +0530

    ok

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,5 @@
 Project::Application.routes.draw do
+  get "Welcome/index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +49,21 @@ Project::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  #root :to => 'welcome#index'
 
-  # See how all your routes lay out with "rake routes"
+# See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+
+Project::Application.routes.draw do
+ 
+  resources :posts
+ 
+  root to: "welcome#index"
+end
+
+
+
+# This is a legacy wild controller route that's not recommended for RESTful applications.
+# Note: This route will make all actions in every controller accessible via GET requests.
+# match ':controller(/:action(/:id))(.:format)'
 end
